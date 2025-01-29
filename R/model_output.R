951b118db27035a41077060921d27981e8ef69ef
sheny2 <shenyicheng1109@gmail.com>
2025-01-28 21:05:02 -0500

    Add model_output function

---
+++ R/model_output.R
@@ -0,0 +1,19 @@
+#' @export
+
+coef.hiperglm <- function(hiperglm_out){
+  warning("This function will be implemented.")
+}
+
+
+#' @export
+
+vcov.hiperglm <- function(hiperglm_out){
+  warning("This function will be implemented.")
+}
+
+
+#' @export
+
+print.hiperglm <- function(hiperglm_out){
+  cat("Output of hiper_glm. \n")
+}
