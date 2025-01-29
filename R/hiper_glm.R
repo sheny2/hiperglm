a1bf93538dadeee2f935c3ebc3782a9bf5757fa2
sheny2 <shenyicheng1109@gmail.com>
2025-01-28 21:02:33 -0500

    Add hiper_glm function

---
+++ R/hiper_glm.R
@@ -0,0 +1,8 @@
+#' @export
+
+hiper_glm <- function(design, outcome){
+  warning("This function will be implemented.")
+  hiperglm_out <- list()
+  class(hiperglm_out) <- "hiperglm"
+  return(hiperglm_out)
+}
